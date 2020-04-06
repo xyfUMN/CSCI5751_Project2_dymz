7a1df0cdff8f6c95f43c0ee8fcba935a8090168b
xyfUMN <55003798+xyfUMN@users.noreply.github.com>
2020-04-05 20:49:11 -0500

    Create Step3a.sql

---
+++ sql_scripts/Step3a.sql
@@ -0,0 +1,26 @@
+SET VAR:database_name=dymz_sales;
+
+Create Database IF NOT EXISTS ${var:database_name}
+COMMENT 'Parquet sales data imported from sales_raw database';
+
+--Create Parquet sales Table
+CREATE TABLE IF NOT EXISTS ${var:database_name}.product_sales_partition
+COMMENT 'Parquet product and sales materialized table'
+STORED AS Parquet
+AS
+(select a.orderid AS OrderID,
+        a.salespersonid AS SalesPersonID,
+        a.customerid AS CustomerID,
+        a.productid AS ProductID,
+        b.name AS ProductName,
+        b.price AS ProductPrice,
+        a.quantity AS Quantity,
+        (a.quantity)*(b.price) AS TotalSalesAmount,
+        day(a.`date`) AS OrderDate,
+        year(a.`date`) AS SalesYear,
+        month(a.`date`) AS SalesMonth
+        from sales a, products b
+        where a.productid = b.productid);
+
+invalidate metadata;
+compute stats ${var:database_name}.product_sales_partition;
