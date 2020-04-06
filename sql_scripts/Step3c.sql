23fe618a4a75109cb9ebf604bcc3c2b3f29e6aaf
xyfUMN <55003798+xyfUMN@users.noreply.github.com>
2020-04-05 22:19:21 -0500

    Create Step3c.sql

---
+++ sql_scripts/Step3c.sql
@@ -0,0 +1,16 @@
+SET VAR:database_name=dymz_sales;
+
+Create Database IF NOT EXISTS ${var:database_name}
+COMMENT 'Parquet sales data imported from sales_raw database';
+
+--Create Parquet sales Table
+CREATE TABLE IF NOT EXISTS ${var:database_name}.product_region_sales_partition
+COMMENT 'Parquet region and sales materialized table'
+STORED AS Parquet
+AS
+(select a.*, b.region
+        from product_sales_partition a, employees b,
+        where a.salespersonid = b.employeeid);
+
+invalidate metadata;
+compute stats ${var:database_name}.product_region_sales_partition;
