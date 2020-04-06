d2d364c5b9ab273ac5f1ea55137a18d355631d52
xyfUMN <55003798+xyfUMN@users.noreply.github.com>
2020-04-05 21:59:45 -0500

    Create Step3b.sql

---
+++ sql_scripts/Step3b.sql
@@ -0,0 +1,10 @@
+SET VAR:database_name=dymz_sales;
+
+CREATE VIEW IF NOT EXISTS ${var:database_name}.customer_monthly_sales_2019_partitioned_view AS
+SELECT a.customerid, b.lastname, b.firstname, a.salesyear, a.salesmonth, a.total
+FROM (
+    SELECT customerid, salesyear, salesmonth, SUM(totalsalesamount) AS total
+    FROM product_sales_partition
+    WHERE salesyear = 2019
+    GROUP BY customerid, salesyear, salesmonth) AS a
+JOIN customers b ON a.customerid = b.customerid
