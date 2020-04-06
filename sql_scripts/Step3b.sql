1b92d816ca79d184c88fa96f8c445bd62772351d
xyfUMN <55003798+xyfUMN@users.noreply.github.com>
2020-04-05 22:00:34 -0500

    Update Step3b.sql

--- sql_scripts/Step3b.sql
+++ sql_scripts/Step3b.sql
@@ -4,7 +4,7 @@ CREATE VIEW IF NOT EXISTS ${var:database_name}.customer_monthly_sales_2019_parti
 SELECT a.customerid, b.lastname, b.firstname, a.salesyear, a.salesmonth, a.total
 FROM (
     SELECT customerid, salesyear, salesmonth, SUM(totalsalesamount) AS total
-    FROM product_sales_partition
+    FROM ${var:database_name}.product_sales_partition
     WHERE salesyear = 2019
     GROUP BY customerid, salesyear, salesmonth) AS a
-JOIN customers b ON a.customerid = b.customerid
+JOIN ${var:database_name}.customers b ON a.customerid = b.customerid
